0f522c928051a9a49dac41cb28c53bd88ebbda91
Parvinder Singh <psndna88@gmail.com>
2019-08-06 14:47:46 +0530

    merge LA.UM.7.2.r1-07400

--- net/wireless/rdev-ops.h
+++ net/wireless/rdev-ops.h
@@ -33,6 +33,20 @@ static inline void rdev_set_wakeup(struct cfg80211_registered_device *rdev,
 //	trace_rdev_return_void(&rdev->wiphy);
 }
 
+static inline int
+rdev_external_auth(struct cfg80211_registered_device *rdev,
+		   struct net_device *dev,
+		   struct cfg80211_external_auth_params *params)
+{
+	int ret = -EOPNOTSUPP;
+
+	trace_rdev_external_auth(&rdev->wiphy, dev, params);
+	if (rdev->ops->external_auth)
+		ret = rdev->ops->external_auth(&rdev->wiphy, dev, params);
+	trace_rdev_return_int(&rdev->wiphy, ret);
+	return ret;
+}
+
 static inline struct wireless_dev
 *rdev_add_virtual_intf(struct cfg80211_registered_device *rdev, char *name,
 		       unsigned char name_assign_type,
