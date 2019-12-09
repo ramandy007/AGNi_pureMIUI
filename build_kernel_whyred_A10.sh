f6d6d6cae2d7684b3823fc9c5891a4802f66244e
Parvinder Singh <psndna88@gmail.com>
2019-12-10 01:22:19 +0530

    Bringup Agni test build

---
+++ build_kernel_whyred_A10.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+export KERNELDIR=`readlink -f .`
+if [ -f ~/WORKING_DIRECTORY/AGNi_stamp.sh ];
+	then
+	. ~/WORKING_DIRECTORY/AGNi_stamp.sh
+fi
+if [ -f ~/WORKING_DIRECTORY/gcc-8.x-uber_aarch64.sh ];
+	then
+	. ~/WORKING_DIRECTORY/gcc-8.x-uber_aarch64.sh
+fi
+
+export ARCH=arm64
+export SUBARCH=arm64
+mkdir -p /mnt/ANDROID/COMPILED_OUT
+
+echo ""
+echo " Cross-compiling AGNi purePIE kernel whyred..."
+echo ""
+
+cd $KERNELDIR/
+
+if [ ! -f /mnt/ANDROID/COMPILED_OUT/.config ];
+then
+    make defconfig O=/mnt/ANDROID/COMPILED_OUT ARCH=arm64 agni_whyred-A10_defconfig
+fi
+
+make -j8 O=/mnt/ANDROID/COMPILED_OUT ARCH=arm64
+
+rm -rf $KERNELDIR/BUILT_whyred-A10
+mkdir -p $KERNELDIR/BUILT_whyred-A10
+
+#find -name '*.ko' -exec mv -v {} $KERNELDIR/BUILT_whyred-A10/system/lib/modules \;
+
+mv /mnt/ANDROID/COMPILED_OUT/arch/arm64/boot/Image.*-dtb $KERNELDIR/BUILT_whyred-A10/
+
+echo ""
+echo "AGNi Android 10 has been built for whyred !!!"
+
