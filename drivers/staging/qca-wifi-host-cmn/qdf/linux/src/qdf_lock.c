9fc802a468a146334955eb4317bb03d3542afa4d
Parvinder Singh <psndna88@gmail.com>
2019-12-17 16:08:39 +0530

    Qcacd-3.0 restore to LA.UM.7.2.r1-07700-sdm660.0

--- drivers/staging/qca-wifi-host-cmn/qdf/linux/src/qdf_lock.c
+++ drivers/staging/qca-wifi-host-cmn/qdf/linux/src/qdf_lock.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014-2019 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2014-2017 The Linux Foundation. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
@@ -19,15 +19,15 @@
 #include <linux/module.h>
 #include <qdf_lock.h>
 #include <qdf_trace.h>
-#include <qdf_module.h>
 
 #include <qdf_types.h>
-#include <i_host_diag_core_event.h>
 #ifdef CONFIG_MCL
-#include <cds_api.h>
+#include <i_host_diag_core_event.h>
 #include <hif.h>
+#include <cds_api.h>
 #endif
 #include <i_qdf_lock.h>
+#include <qdf_module.h>
 
 /**
  * qdf_mutex_create() - Initialize a mutex
@@ -240,10 +240,19 @@ qdf_export_symbol(qdf_mutex_release);
  *
  * Return: Pointer to the name if it is valid or a default string
  */
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(3, 10, 0))
+const char *qdf_wake_lock_name(qdf_wake_lock_t *lock)
+{
+	if (lock->name)
+		return lock->name;
+	return "UNNAMED_WAKELOCK";
+}
+#else
 const char *qdf_wake_lock_name(qdf_wake_lock_t *lock)
 {
 	return "NO_WAKELOCK_SUPPORT";
 }
+#endif
 qdf_export_symbol(qdf_wake_lock_name);
 
 /**
@@ -255,10 +264,18 @@ qdf_export_symbol(qdf_wake_lock_name);
  * QDF status success: if wake lock is initialized
  * QDF status failure: if wake lock was not initialized
  */
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(3, 10, 0))
+QDF_STATUS qdf_wake_lock_create(qdf_wake_lock_t *lock, const char *name)
+{
+	wakeup_source_init(lock, name);
+	return QDF_STATUS_SUCCESS;
+}
+#else
 QDF_STATUS qdf_wake_lock_create(qdf_wake_lock_t *lock, const char *name)
 {
 	return QDF_STATUS_SUCCESS;
 }
+#endif
 qdf_export_symbol(qdf_wake_lock_create);
 
 /**
@@ -270,10 +287,23 @@ qdf_export_symbol(qdf_wake_lock_create);
  * QDF status success: if wake lock is acquired
  * QDF status failure: if wake lock was not acquired
  */
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(3, 10, 0))
+QDF_STATUS qdf_wake_lock_acquire(qdf_wake_lock_t *lock, uint32_t reason)
+{
+#ifdef CONFIG_MCL
+	host_diag_log_wlock(reason, qdf_wake_lock_name(lock),
+			WIFI_POWER_EVENT_DEFAULT_WAKELOCK_TIMEOUT,
+			WIFI_POWER_EVENT_WAKELOCK_TAKEN);
+#endif
+	__pm_stay_awake(lock);
+	return QDF_STATUS_SUCCESS;
+}
+#else
 QDF_STATUS qdf_wake_lock_acquire(qdf_wake_lock_t *lock, uint32_t reason)
 {
 	return QDF_STATUS_SUCCESS;
 }
+#endif
 qdf_export_symbol(qdf_wake_lock_acquire);
 
 /**
@@ -285,10 +315,21 @@ qdf_export_symbol(qdf_wake_lock_acquire);
  * QDF status success: if wake lock is acquired
  * QDF status failure: if wake lock was not acquired
  */
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(3, 10, 0))
+QDF_STATUS qdf_wake_lock_timeout_acquire(qdf_wake_lock_t *lock, uint32_t msec)
+{
+	/* Wakelock for Rx is frequent.
+	 * It is reported only during active debug
+	 */
+	__pm_wakeup_event(lock, msec);
+	return QDF_STATUS_SUCCESS;
+}
+#else
 QDF_STATUS qdf_wake_lock_timeout_acquire(qdf_wake_lock_t *lock, uint32_t msec)
 {
 	return QDF_STATUS_SUCCESS;
 }
+#endif
 qdf_export_symbol(qdf_wake_lock_timeout_acquire);
 
 /**
@@ -300,10 +341,23 @@ qdf_export_symbol(qdf_wake_lock_timeout_acquire);
  * QDF status success: if wake lock is acquired
  * QDF status failure: if wake lock was not acquired
  */
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(3, 10, 0))
+QDF_STATUS qdf_wake_lock_release(qdf_wake_lock_t *lock, uint32_t reason)
+{
+#ifdef CONFIG_MCL
+	host_diag_log_wlock(reason, qdf_wake_lock_name(lock),
+			WIFI_POWER_EVENT_DEFAULT_WAKELOCK_TIMEOUT,
+			WIFI_POWER_EVENT_WAKELOCK_RELEASED);
+#endif
+	__pm_relax(lock);
+	return QDF_STATUS_SUCCESS;
+}
+#else
 QDF_STATUS qdf_wake_lock_release(qdf_wake_lock_t *lock, uint32_t reason)
 {
 	return QDF_STATUS_SUCCESS;
 }
+#endif
 qdf_export_symbol(qdf_wake_lock_release);
 
 /**
@@ -314,17 +368,25 @@ qdf_export_symbol(qdf_wake_lock_release);
  * QDF status success: if wake lock is acquired
  * QDF status failure: if wake lock was not acquired
  */
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(3, 10, 0))
+QDF_STATUS qdf_wake_lock_destroy(qdf_wake_lock_t *lock)
+{
+	wakeup_source_trash(lock);
+	return QDF_STATUS_SUCCESS;
+}
+#else
 QDF_STATUS qdf_wake_lock_destroy(qdf_wake_lock_t *lock)
 {
 	return QDF_STATUS_SUCCESS;
 }
+#endif
 qdf_export_symbol(qdf_wake_lock_destroy);
 
 #ifdef CONFIG_MCL
 /**
  * qdf_runtime_pm_get() - do a get opperation on the device
  *
- * A get opperation will prevent a runtime suspend until a
+ * A get opperation will prevent a runtime suspend untill a
  * corresponding put is done.  This api should be used when sending
  * data.
  *
@@ -480,44 +542,8 @@ qdf_export_symbol(__qdf_runtime_lock_init);
 void qdf_runtime_lock_deinit(qdf_runtime_lock_t *lock)
 {
 	void *hif_ctx = cds_get_context(QDF_MODULE_ID_HIF);
+
 	hif_runtime_lock_deinit(hif_ctx, lock->lock);
-}
-qdf_export_symbol(qdf_runtime_lock_deinit);
-
-#else
-
-QDF_STATUS qdf_runtime_pm_get(void)
-{
-	return QDF_STATUS_SUCCESS;
-}
-qdf_export_symbol(qdf_runtime_pm_get);
-
-QDF_STATUS qdf_runtime_pm_put(void)
-{
-	return QDF_STATUS_SUCCESS;
-}
-qdf_export_symbol(qdf_runtime_pm_put);
-
-QDF_STATUS qdf_runtime_pm_prevent_suspend(qdf_runtime_lock_t *lock)
-{
-	return QDF_STATUS_SUCCESS;
-}
-qdf_export_symbol(qdf_runtime_pm_prevent_suspend);
-
-QDF_STATUS qdf_runtime_pm_allow_suspend(qdf_runtime_lock_t *lock)
-{
-	return QDF_STATUS_SUCCESS;
-}
-qdf_export_symbol(qdf_runtime_pm_allow_suspend);
-
-QDF_STATUS __qdf_runtime_lock_init(qdf_runtime_lock_t *lock, const char *name)
-{
-	return QDF_STATUS_SUCCESS;
-}
-qdf_export_symbol(__qdf_runtime_lock_init);
-
-void qdf_runtime_lock_deinit(qdf_runtime_lock_t *lock)
-{
 }
 qdf_export_symbol(qdf_runtime_lock_deinit);
 
