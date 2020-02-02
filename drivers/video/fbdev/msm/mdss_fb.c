9f94a0099351d0fa71e66f98a8e5c3ccac888d2d
psndna88 <psndna88@gmail.com>
2020-02-02 23:30:28 +0530

    Brihtness changes

--- drivers/video/fbdev/msm/mdss_fb.c
+++ drivers/video/fbdev/msm/mdss_fb.c
@@ -105,7 +105,14 @@ bool backlight_dimmer = false;
 module_param(backlight_dimmer, bool, 0644);
 
 int backlight_min = 0;
+//#ifdef CONFIG_MACH_XIAOMI_AGNI_MIUI
+int backlight_max = MDSS_MAX_BL_BRIGHTNESS_MIUI;
+//#else
+//int backlight_max = MDSS_MAX_BL_BRIGHTNESS;
+//#endif
 module_param(backlight_min, int, 0644);
+module_param(backlight_max, int, 0644);
+
 
 static struct fb_info *fbi_list[MAX_FBI_LIST];
 static int fbi_list_index;
@@ -348,14 +355,20 @@ static void mdss_fb_set_bl_brightness(struct led_classdev *led_cdev,
 	if (value > mfd->panel_info->brightness_max)
 		value = mfd->panel_info->brightness_max;
 
-	// Boeffla: apply min limits for LCD backlight (0 is exception for display off)
-	if (value != 0 && value < backlight_min)
-		value = backlight_min;
+	// Boeffla: apply min/max limits for LCD backlight (0 is exception for display off)
+	if (value != 0)
+	{
+		if (value < backlight_min)
+			value = backlight_min;
+
+		if (value > backlight_max)
+			value = backlight_max;
+	}
 
 	if (backlight_dimmer) {
 		MDSS_BRIGHT_TO_BL_DIM(bl_lvl, value);
 	} else {
-		/* This maps android backlight level 0 to 255 into
+		/* This maps android backlight level 0 to 255 or 4095 into
 		   driver backlight level 0 to bl_max with rounding */
 		MDSS_BRIGHT_TO_BL(bl_lvl, value, mfd->panel_info->bl_max,
 					mfd->panel_info->brightness_max);
